77ba1487a5d564cc22e6b02211213099aec8219a
indiacovid19info <namaninaresh5566@gmail.com>
2020-06-24 20:59:59 +0530

    darkk

---
+++ precache-manifest.dea5c1fa9a5b550609e943c9241fe246.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee47acb34846f236fc929abb78d9d30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af44f72eb88a53a1e699",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a129472a4754da89f7b5",
+    "url": "/static/css/main.632b5de6.chunk.css"
+  },
+  {
+    "revision": "af44f72eb88a53a1e699",
+    "url": "/static/js/2.49b682d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.49b682d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a129472a4754da89f7b5",
+    "url": "/static/js/main.79caac57.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47849991843d0985b4ce982f353de872.js"
+  "/precache-manifest.dea5c1fa9a5b550609e943c9241fe246.js"
 );
 
 self.addEventListener('message', (event) => {
