4558eb8af597046b96cecc573fc4b0546f567cc2
indiacovid19info <namaninaresh5566@gmail.com>
2020-06-24 23:02:57 +0530

    UiChange

---
+++ precache-manifest.4634b5cd7a970979c9a9cbe4d33d4d42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc379ee2c6cd9c4c95780dcb8afd7cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5297b512c560afb8d820",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c95fe5591e61d6d9a37f",
+    "url": "/static/css/main.4c02d8ed.chunk.css"
+  },
+  {
+    "revision": "5297b512c560afb8d820",
+    "url": "/static/js/2.e0097748.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.e0097748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c95fe5591e61d6d9a37f",
+    "url": "/static/js/main.a4f2c0f3.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
